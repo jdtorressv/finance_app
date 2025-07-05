1e4db0690ba8aceab0140d38cd91cf6d1f685967
Joseph Torres <jdtorressv@gmail.com>
2025-07-05 12:51:26 -0700

    simple event handling

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,24 @@
-import React, { JSX, useState } from 'react'
+import React, { JSX, SyntheticEvent, useState } from 'react'
 
 type Props = {}
 
 const Search : React.FC<Props> = (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>('');
 
-const onClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
 }
 
+const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+}
+
   return (
-    <div><input value={search} onChange={(e) => onClick(e)}></input></div>
+    <div>
+        <input value={search} onChange={(e) => handleChange(e)}></input>
+        <button onClick={(e) => onClick(e)}/>
+    </div>
   )
 }
 
