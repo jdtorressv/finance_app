63cc889aa29b4875aac897d024df89047fa47709
Joseph Torres <jdtorressv@gmail.com>
2025-07-05 13:54:20 -0700

    Set up axios

---
+++ frontend/src/api.tsx
@@ -0,0 +1,26 @@
+
+import axios, {isAxiosError} from 'axios';
+import { CompanySearch } from './company';
+
+interface SearchResponse {
+    data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+    try {
+        const data = await axios.get<SearchResponse>(
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`);
+        return data;
+    }
+
+    catch (error) {
+        if (isAxiosError(error)) {
+            console.log('Axios error message: ', error.message);
+            return error.message;
+        }
+        else {
+            console.log('Unexpected error: ', error);
+            return 'An unexpected error occurred';
+        }
+    }
+}
