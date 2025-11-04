3d33131069cfa38c8868620346e9299361146247
Joseph Torres <jdtorressv@gmail.com>
2025-11-04 12:19:20 -0800

    tailwind css

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -19,7 +19,9 @@ const CardList: React.FC<Props> = ({searchResults, onPortfolioCreate}: Props): J
           onPortfolioCreate={onPortfolioCreate} 
         />
       }
-      )): <h1>No Results Found</h1>}
+      )): <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+            No results!
+          </p>}
     </div>
   )
 }
